04aed2280648577f6f928616c1c0f9108f588f3b
Upasana Chaudhary <upasnachoudhary216@gmail.com>
2024-04-05 09:33:11 +0530

    second commit

---
+++ day 3/js.js
@@ -0,0 +1,84 @@
+
+// let s="upasana"
+//     console.log("hello"+ s)
+// let age="20";
+// console.log(typeof(age));
+
+// let a='abcd';
+// a='abbd';
+// console.log(a);
+
+
+// let str="12";
+// let age="20";
+// let ans=age-(-str);
+// console.log(ans);
+// ans=str*age;
+// console.log(ans);
+
+// 
+
+// let str="upasana"
+// const n="my name is : ";
+// let ans=`${n} ${str}`
+// console.log(ans)
+
+
+
+// let printf=(str)=>
+// {
+//     console.log(str);
+// }
+// printf("hello");
+
+//function declaration
+// function printf()
+// {
+//     console.log('--');
+// }
+
+// //function assignment(named)
+// const a=function printf()
+// {
+//     console.log('--');
+// }
+
+// //function assignment(anonmymos)
+// const b=function ()
+// {
+//     console.log('--');
+// }
+
+// printf();
+// a();
+// b();
+
+
+// return from arrow function
+// let sum=(a,b)=>
+// {
+//     return a+b;
+// }
+
+// let temp=sum(2,4);
+// console.log(temp);
+
+
+// const obj=new Object();
+// obj[12]="twelve"
+// console.log(obj);
+
+//  const obj={};
+//  obj[12]="twelve"
+// console.log(obj);
+
+
+const obj={
+    1:'one',
+    'age':'21',
+    12:"twelve",
+};
+let x=prompt("property:")
+console.log(obj[x]);
+
+
